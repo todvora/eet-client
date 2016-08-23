173a245c03d4f0d69eceb8c8f92e1b6fc11a4d1f
Tomas Dvorak <todvora@gmail.com>
2016-08-23 20:17:51 +0200

    EET v3.0 WSDL update

--- src/main/java/cz/tomasdvorak/eet/client/config/EndpointType.java
+++ src/main/java/cz/tomasdvorak/eet/client/config/EndpointType.java
@@ -2,7 +2,7 @@ package cz.tomasdvorak.eet.client.config;
 
 public enum EndpointType {
     // TODO: add real endpoint, when known
-    PLAYGROUND("https://pg.eet.cz:443/eet/services/EETServiceSOAP/v2");
+    PLAYGROUND("https://pg.eet.cz:443/eet/services/EETServiceSOAP/v3");
 
     private final String webserviceUrl;
 
