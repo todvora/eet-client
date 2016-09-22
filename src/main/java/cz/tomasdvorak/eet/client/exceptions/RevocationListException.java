a6eba1630b72fc71d707dad22bea98c099662b06
Tomas Dvorak <todvora@gmail.com>
2016-09-22 18:04:33 +0200

    RevocationListException: added message

--- src/main/java/cz/tomasdvorak/eet/client/exceptions/RevocationListException.java
+++ src/main/java/cz/tomasdvorak/eet/client/exceptions/RevocationListException.java
@@ -1,6 +1,11 @@
 package cz.tomasdvorak.eet.client.exceptions;
 
 public class RevocationListException extends Exception {
+
+    public RevocationListException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
     public RevocationListException(final Throwable cause) {
         super(cause);
     }
