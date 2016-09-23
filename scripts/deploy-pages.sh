1437b3974928839275467c9012385c96df3b466b
Tomas Dvorak <todvora@gmail.com>
2016-09-23 14:04:16 +0200

    deploy script fix

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -14,5 +14,5 @@ cd target/site
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet https://${GH_TOKEN}@${GH_REF} master:gh-pages > /dev/null 2>&1
 
