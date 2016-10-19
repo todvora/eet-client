cf40af2fd9a942ecad320385a171e2c85adbd335
Tomas Dvorak <todvora@gmail.com>
2016-10-19 19:24:58 +0200

    update version script

---
+++ scripts/update-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -o errexit
+
+# https://blog.codecentric.de/en/2015/04/increment-versions-maven-build-helper-versions-plugin/
+
+if [ $1 = "major" ]; then
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0 versions:commit
+elif [ $1 = "minor" ]; then
+  mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0 versions:commit
+elif [ $1 = "patch" ]; then
+  mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+else
+    echo "Unknown component increment: $1"
+    exit 1
+fi
+
+NEW_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
+
+git add pom.xml
+
+git commit -m "release $NEW_VERSION"
+git tag -fa $NEW_VERSION -m "release $NEW_VERSION"
+
+git push --follow-tags
