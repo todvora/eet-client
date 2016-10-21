7d1a2f385da56d3f5ae4e5bc3310cd23d8783f7d
Tomas Dvorak <todvora@gmail.com>
2016-10-21 14:08:17 +0200

    CRLs downloaded parallel - faster certificate validation

--- src/main/java/cz/tomasdvorak/eet/client/security/crl/InMemoryCRLStore.java
+++ src/main/java/cz/tomasdvorak/eet/client/security/crl/InMemoryCRLStore.java
@@ -1,7 +1,6 @@
 package cz.tomasdvorak.eet.client.security.crl;
 
 import cz.tomasdvorak.eet.client.exceptions.RevocationListException;
-import cz.tomasdvorak.eet.client.security.ServerKey;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
@@ -19,25 +18,31 @@ import java.security.cert.X509CRL;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class InMemoryCRLStore {
 
+    private static final int CRL_RETRIEVE_TIMEOUT_MILLIS = 2000;
+
     public static final InMemoryCRLStore INSTANCE = new InMemoryCRLStore();
 
-    private static final Map<URI, X509CRL> CACHE = new ConcurrentHashMap<URI, X509CRL>();
+    private static final Map<URI, X509CRL> CACHE = new HashMap<URI, X509CRL>();
 
     private static final Logger logger = org.apache.logging.log4j.LogManager.getLogger(InMemoryCRLStore.class);
 
     public CertStore getCRLStore(final X509Certificate[] certificates) throws RevocationListException {
         final List<X509CRL> x509CRLs = new ArrayList<X509CRL>();
         for(final X509Certificate cert : certificates) {
-            final List<URI> uris = CRLUtils.getCRLs(cert);
-            for(final URI uri : uris) {
-                x509CRLs.add(getCRL(uri));
-            }
+            x509CRLs.addAll(getCrls(cert));
         }
         try {
             return CertStore.getInstance("Collection", new CollectionCertStoreParameters(x509CRLs));
@@ -48,7 +53,35 @@ public class InMemoryCRLStore {
         }
     }
 
-    private X509CRL getCRL(final URI address) throws RevocationListException {
+    private List<X509CRL> getCrls(final X509Certificate cert) throws RevocationListException {
+        final List<X509CRL> result = new ArrayList<X509CRL>();
+        final List<URI> uris = CRLUtils.getCRLs(cert);
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+        final List<Future<X509CRL>> futures = new ArrayList<Future<X509CRL>>();
+        for (final URI uri : uris) {
+            futures.add(executorService.submit(new Callable<X509CRL>() {
+                @Override
+                public X509CRL call() throws Exception {
+                    return getCRL(uri);
+                }
+            }));
+        }
+        for (final Future<X509CRL> future : futures) {
+            try {
+                final X509CRL x509CRL = future.get(CRL_RETRIEVE_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+                result.add(x509CRL);
+            } catch (final InterruptedException e) {
+                throw new RevocationListException(e);
+            } catch (final ExecutionException e) {
+                throw new RevocationListException(e);
+            } catch (final TimeoutException e) {
+                throw new RevocationListException(e);
+            }
+        }
+        return result;
+    }
+
+    private synchronized X509CRL getCRL(final URI address) throws RevocationListException {
         if(CACHE.containsKey(address)) {
             final X509CRL x509CRL = CACHE.get(address);
             final Date nextUpdate = x509CRL.getNextUpdate();
@@ -59,13 +92,13 @@ public class InMemoryCRLStore {
                 logger.debug("CRL from URI " + address.toString() + " is stale, updating now");
             }
         }
-        final X509CRL x509CRL = loadCRL(address);
+        final X509CRL x509CRL = downloadCRL(address);
         logger.info("CRL loaded from URI " + address.toString() + ", storing in cache. Next update: " + x509CRL.getNextUpdate());
         CACHE.put(address, x509CRL);
         return x509CRL;
     }
 
-    private X509CRL loadCRL(final URI address) throws RevocationListException {
+    private X509CRL downloadCRL(final URI address) throws RevocationListException {
         try {
             final URL url = new URL(address.toString());
             final CertificateFactory cf = CertificateFactory.getInstance("X.509");
