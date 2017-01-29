8ffdeb145466365f6ff357e09cdefaf5064656e1
Tomas Dvorak <todvora@gmail.com>
2017-01-29 19:53:50 +0100

    better gh-pages deployment (preserves older releases documentation)

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -9,10 +9,36 @@ git config --global user.name "${GH_USERNAME}"
 # build site
 mvn site
 
-# deploy
-cd target/site
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet https://${GH_TOKEN}@${GH_REF} master:gh-pages > /dev/null 2>&1
+# get the current version from pom.xml
+CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
+
+# clone current github pages
+git clone -b gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages
+
+# prepare a directory for current version pages
+mkdir -p gh-pages/$CURRENT_VERSION
+
+# remove any content of this version, if exists
+rm -rf gh-pages/$CURRENT_VERSION/*  2> /dev/null
+
+# copy generated maven site to a subdirectory
+cp -r target/site/* gh-pages/$CURRENT_VERSION/
+
+# disable jekyll build of this page
+touch gh-pages/.nojekyll
+
+cd gh-pages
+
+# remove any symlink to the latest version, if exists
+rm -f latest 2> /dev/null
+# create new symlink latest to this version
+ln -s $CURRENT_VERSION latest
+
+# create directory listening
+# See https://little418.com/2015/04/directory-listings-on-github-pages.html
+ls -d */ | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+
+git add -A .
+git commit -m "Deploy $CURRENT_VERSION to Github Pages"
+git push --quiet https://${GH_TOKEN}@${GH_REF} gh-pages:gh-pages > /dev/null 2>&1
 
