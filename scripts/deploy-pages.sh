c9e7284e7d03e123ee2f7a50b1baa239221b2a4f
Tomas Dvorak <todvora@gmail.com>
2017-01-29 20:16:27 +0100

    gh-pages release - added more logging

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -8,24 +8,30 @@ git config --global user.name "${GH_USERNAME}"
 
 # build site
 mvn site
-
+echo "mvn site finished building documentation"
 # get the current version from pom.xml
+echo "reading current project version from pom.xml"
 CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
 echo -e "Preparing gh-pages for version $CURRENT_VERSION"
 
 # clone current github pages
+echo "cloning gh-pages branch"
 git clone -b gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages
 
 # prepare a directory for current version pages
+echo "preparing directory gh-pages/$CURRENT_VERSION for documentation"
 mkdir -p gh-pages/$CURRENT_VERSION
 
 # remove any content of this version, if exists
+echo "removing all content of gh-pages/$CURRENT_VERSION if exists"
 rm -rf gh-pages/$CURRENT_VERSION/*  2> /dev/null
 
 # copy generated maven site to a subdirectory
+echo "copying all files from target/site/* to gh-pages/$CURRENT_VERSION/"
 cp -r target/site/* gh-pages/$CURRENT_VERSION/
 
 # disable jekyll build of this page
+echo "touching gh-pages/.nojekyll to disable jekyll pages build"
 touch gh-pages/.nojekyll
 
 cd gh-pages
